31da432976e92257519449839ee9ce514067331f
zukethenuke <zukethenuke@hotmail.com>
2017-12-05 21:22:35 -0600

    Add searchValue to localStorage and add clear search button

--- src/components/search/search.controller.js
+++ src/components/search/search.controller.js
@@ -4,12 +4,19 @@ class SearchController {
     }
 
     $onInit() {
-        this.searchValue = '';
+        if (localStorage.getItem("searchValue")) { this.listFromLocalStorage() };
+    }
+
+    listFromLocalStorage() {
+        this.searchValue = localStorage.getItem("searchValue");
+        this.getWellList();
     }
 
     getWellList() {
         this.message = '';
         const searchValue = this.searchValue;
+
+        localStorage.setItem("searchValue", searchValue);
 
         this.$http({
             method: 'GET',
@@ -19,12 +26,21 @@ class SearchController {
         }).then((response) => this.formatRseponse(response));
     }
 
+    clearSearch() {
+        this.searchValue = '';
+        this.message = '';
+        this.wells = [];
+        localStorage.setItem("searchValue", '');
+    }
+
     formatRseponse(response) {
         if (response.data.error) {
             this.wells = [];
             this.message = response.data.error;
         }else {
             this.wells = response.data.wells;
+            // console.log(this.wells);
+            // localStorage.setItem("wells", this.wells);
         };
     }
 }
