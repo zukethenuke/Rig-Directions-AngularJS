1f570035910b87d4d4523628740a2f9d4bf71eb4
zukethenuke <zukethenuke@hotmail.com>
2017-12-17 21:11:30 -0600

    Add ng-cloak to map

--- src/components/rd-well-show/rd-well-show.controller.js
+++ src/components/rd-well-show/rd-well-show.controller.js
@@ -2,11 +2,9 @@ class RdWellShowController {
     constructor($scope) {
         this.well = $scope.$parent.$resolve.well;
         this.mapUrl = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyDKGkdynbpEe2Vq2AJaNGxtxiDjtpyPFSE&origin=Williston+ND&destination=" + this.well.latitude + "," + this.well.longitude;
-        this.mapIsReady = false;
     }
     
     $onInit() {
-        this.mapIsReady = true;
     }
                 
 }
